9173d442abfaaa9224754b98e698b6f5fee6d0ed
NatanTavares <nattantavares.s15@gmail.com>
2021-07-16 21:52:54 -0300

    feat(global): Implements migration from Js to Ts

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,14 +3,19 @@ import {
   useRef,
   useState,
   useCallback,
-} from 'react';
+  InputHTMLAttributes,
+} from "react";
+import { useField } from "@unform/core";
 
-import { useField } from '@unform/core';
+import { Container } from "./styles";
 
-import { Container } from './styles';
+type Props = InputHTMLAttributes<HTMLInputElement> & {
+  name: string;
+  icon?: any;
+};
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+export function Input({ name, icon: Icon, ...rest }: Props) {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -23,7 +28,6 @@ const Input = ({ name, icon: Icon, ...rest }) => {
 
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
-
     setIsFilled(!!inputRef.current?.value);
   }, []);
 
@@ -31,7 +35,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     registerField({
       name: fieldName,
       ref: inputRef.current,
-      path: 'value',
+      path: "value",
     });
   }, [fieldName, registerField]);
 
@@ -48,6 +52,4 @@ const Input = ({ name, icon: Icon, ...rest }) => {
       />
     </Container>
   );
-};
-
-export default Input;
+}
